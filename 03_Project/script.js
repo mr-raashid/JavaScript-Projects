1fd959cdd4b294cc88cbf57be6086eed21a6a565
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-03-17 19:15:43 +0000

    project-03

--- 03_Project/script.js
+++ 03_Project/script.js
@@ -3,16 +3,28 @@ const generateBtn = document.querySelector("#btn");
 
 generateBtn.addEventListener("click", () => {
     let url = fetch("https://dummyjson.com/quotes");
-    url.then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        const randomNum = Math.floor(Math.random () * data.quotes.length);
+    // url.then((res) => {
+    //     return res.json();
+    // })
+    // .then((data) => {
+    //     const randomNum = Math.floor(Math.random () * data.quotes.length);
+    //     console.log(randomNum);
+    //     const randomQuotes = data.quotes[randomNum].quote
+    //     textArea.innerHTML = randomQuotes;
+    // })
+    // .catch((error) => {
+    //     console.log(error);
+    // })
+    // .finally(() => {
+    //     console.log("Successfully fetched the data");
+    // })
+    async function randomQuotes () {
+        const respone = await (url);
+        const data = await respone.json();
+        let randomNum = Math.floor(Math.random() * data.quotes.length);
         console.log(randomNum);
-        const randomQuotes = data.quotes[randomNum].quote
-        textArea.innerHTML = randomQuotes;
-    })
-    .catch((error) => {
-        console.log(error);
-    })  
+        textArea.innerHTML = data.quotes[randomNum].quote;
+    }
+    randomQuotes();
+
 })
